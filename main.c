039eb6eca72f81284e53e5f5944650ccd936d7df
Vincent Jiang <vincentjiang2003@gmail.com>
2020-09-26 13:57:55 -0400

    modified main.c

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	printf("Good Morning\n");
+	// testing prinf
+	printf("I would like get a good night's sleep during the weekends\n");
+	int a = 22;
+	int b = 7;
+	float pi = 3.1415;
+	printf("A fair approximation of pi%c%c%0.4f%c is %d/%d\n", '(', '~', pi, ')', a, b);
 
+	//testing variable types
+
+	//  
 	return 0;
 }
